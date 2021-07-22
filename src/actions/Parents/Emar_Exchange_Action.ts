22b4daa5f3bc9009648c8575018b2d2829b27c91
Joseph Garrigan <jgarrigan@terpsrx.com>
2021-07-22 10:58:49 -0400

    fixing typo that left fakemar actions out of the emar exchange application

--- src/actions/Parents/Emar_Exchange_Action.ts
+++ src/actions/Parents/Emar_Exchange_Action.ts
@@ -16,5 +16,6 @@ export abstract class Emar_Exchange_Action extends Application_Action {
     super();
     this.name = "Emar_Exchange_Action";
     this.description = "Adds emar_name and emar_id properties to the Application_Action class";
+    this.application_name = "Emar_Exchange";
   }
 }
